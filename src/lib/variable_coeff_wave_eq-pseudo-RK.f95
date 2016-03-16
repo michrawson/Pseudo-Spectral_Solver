c39dbcd6cc1b3c0621499a96860cb41eeb9be460
Mich <mr4209@nyu.edu>
2016-03-16 18:02:07 -0400

    advection equation: rotation

--- src/lib/variable_coeff_wave_eq-pseudo-RK.f95
+++ src/lib/variable_coeff_wave_eq-pseudo-RK.f95
@@ -7,7 +7,7 @@ contains
     use fft_prime
     implicit none
         integer, parameter          :: n = 128
-        integer, parameter          :: tmax = 8
+        integer, parameter          :: tmax = 4
         real ( kind = 8 ), parameter :: tplot = 0.15
 
         real ( kind = 8 ), dimension(n), intent(out)                   :: x, y
@@ -24,23 +24,21 @@ contains
         x = h*(/ (j-n/2,j=1,N) /)
         y = h*(/ (j-n/2,j=1,N) /)
         t = 0
-        dt = h/4.
+        dt = h/4./10.
 
         do i=1,n
             do j = 1,n
-                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/0.1)**2.)) + exp(-(((x(i)+pi/6)/0.1)**2.)-((y(j)/0.1)**2.))
+                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/0.1)**2.))
+!                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/0.1)**2.)) + exp(-(((x(i)+pi/6)/0.1)**2.)-((y(j)/0.1)**2.))
             end do
         end do
 
         do i=1,n
-!            prime_phi_x(:,i) = 1
             prime_phi_x(:,i) = 2. * x
-!            prime_phi_y(i,:) = 1
             prime_phi_y(i,:) = 2. * y
         end do
 
         plotgap = nint(tplot/dt)
-        dt = tplot/plotgap
         nplots = nint(tmax/tplot)
 
         tdata = -1
@@ -55,28 +53,19 @@ contains
 
                 call fft_prime_2d_partial_x_run(v, prime_x)
                 call fft_prime_2d_partial_y_run(v, prime_y)
-
-                k1 = prime_phi_y*prime_x - prime_phi_x*prime_y
-
-!                k1 = 2. * spread(y, dim=1, ncopies=n) * prime_x - 2. * spread(x, dim=1, ncopies=n) * prime_y
+                k1 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
 
                 call fft_prime_2d_partial_x_run(v + dt*k1/2, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k1/2, prime_y)
-                k2 = prime_phi_y*prime_x - prime_phi_x*prime_y
-
-!                k2 = 2. * spread(y, dim=1, ncopies=n) * prime_x - 2. * spread(x, dim=1, ncopies=n) * prime_y
+                k2 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
 
                 call fft_prime_2d_partial_x_run(v + dt*k2/2, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k2/2, prime_y)
-                k3 = prime_phi_y*prime_x - prime_phi_x*prime_y
-
-!                k3 = 2. * spread(y, dim=1, ncopies=n) * prime_x - 2. * spread(x, dim=1, ncopies=n) * prime_y
+                k3 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
 
                 call fft_prime_2d_partial_x_run(v + dt*k3, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k3, prime_y)
-                k4 = prime_phi_y*prime_x - prime_phi_x*prime_y
-
-!                k4 = 2. * spread(y, dim=1, ncopies=n) * prime_x - 2. * spread(x, dim=1, ncopies=n) * prime_y
+                k4 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
 
                 v = v + dt/6.0*(k1 + 2.0*k2 + 2.0*k3 + k4)
 
