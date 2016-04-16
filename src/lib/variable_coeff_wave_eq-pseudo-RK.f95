a17a530ac8893f3207554a4778cfb84aa6f8f03d
Mich <mr4209@nyu.edu>
2016-04-16 00:18:23 -0400

    elongated_beam

--- src/lib/variable_coeff_wave_eq-pseudo-RK.f95
+++ src/lib/variable_coeff_wave_eq-pseudo-RK.f95
@@ -9,40 +9,38 @@ contains
     implicit none
         integer, parameter          :: n = 128
         integer, parameter          :: tmax = 4
-        real ( kind = 8 ), parameter :: tplot = 0.15
+        integer, parameter          :: nplots = 10
+        real ( kind = 8 ), parameter :: tplot = 0.2
 
         real ( kind = 8 ), dimension(n), intent(out)                   :: x, y
         real ( kind = 8 ), dimension(int(tmax/tplot)+1), intent(out)   :: tdata
-        real ( kind = 8 ), dimension(int(tmax/tplot)+1,n,n), intent(out) :: result
+        real ( kind = 8 ), dimension(10+1,n,n), intent(out) :: result
 
         real ( kind = 8 ), dimension(n,n) :: v, k1,k2,k3,k4
         real ( kind = 8 ), dimension(n,n) :: vx, vy
-        real ( kind = 8 ), dimension(n,n) :: prime_x,prime_y,prime_phi_x,prime_phi_y
+        real ( kind = 8 ), dimension(n,n) :: prime_x,prime_y
 
-        integer               :: i, j, plotgap, nplots
-        real ( kind = 8 )      :: pi, h, t, dt, curr_max, prev_max
+        integer               :: i, j, plotgap
+!        , nplots
+        real ( kind = 8 )      :: pi, h, t, dt, curr_max, prev_max, delta2
 
         pi = 4.*atan(1.)
         h = 2.*pi/N
         x = h*(/ (j-n/2,j=1,N) /)
         y = h*(/ (j-n/2,j=1,N) /)
         t = 0
-        dt = h/4./10.
+        dt = h/4./20.
+        delta2 = 4.0
 
         do i=1,n
             do j = 1,n
-                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/0.1)**2.))
+                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/1.0)**2.))
 !                v(i,j) = exp(-(((x(i)-pi/6)/0.1)**2.)-((y(j)/0.1)**2.)) + exp(-(((x(i)+pi/6)/0.1)**2.)-((y(j)/0.1)**2.))
             end do
         end do
 
-        do i=1,n
-            prime_phi_x(:,i) = 2. * x
-            prime_phi_y(i,:) = 2. * y
-        end do
-
+!        nplots = 20
         plotgap = nint(tplot/dt)
-        nplots = nint(tmax/tplot)
 
         tdata = -1
         tdata(1) = t
@@ -50,35 +48,37 @@ contains
         result = 0
         result(1,1:n,1:n) = v
 
-        prev_max = max_finder_run(maxloc(v), v)
+!        prev_max = max_finder_run(maxloc(v), v)
 
         do i=1, nplots
+            print *,"nplot #",i
             do j = 1,plotgap
+                print *,"plotgap #",j
                 t = t+dt
 
                 call fft_prime_2d_partial_x_run(v, prime_x)
                 call fft_prime_2d_partial_y_run(v, prime_y)
                 call poisson2df(n,n,h,h,v,vx,vy,1)
 
-                k1 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
+                k1 = delta2*(-1.0 * (vy * prime_x) - (vx * prime_y))
 
                 call fft_prime_2d_partial_x_run(v + dt*k1/2, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k1/2, prime_y)
                 call poisson2df(n,n,h,h,v,vx,vy,1)
 
-                k2 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
+                k2 = delta2*(-1.0 * (vy * prime_x) - (vx * prime_y))
 
                 call fft_prime_2d_partial_x_run(v + dt*k2/2, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k2/2, prime_y)
                 call poisson2df(n,n,h,h,v,vx,vy,1)
 
-                k3 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
+                k3 = delta2*(-1.0 * (vy * prime_x) - (vx * prime_y))
 
                 call fft_prime_2d_partial_x_run(v + dt*k3, prime_x)
                 call fft_prime_2d_partial_y_run(v + dt*k3, prime_y)
                 call poisson2df(n,n,h,h,v,vx,vy,1)
 
-                k4 = (prime_phi_y * prime_x) - (prime_phi_x * prime_y)
+                k4 = delta2*(-1.0 * (vy * prime_x) - (vx * prime_y))
 
                 v = v + dt/6.0*(k1 + 2.0*k2 + 2.0*k3 + k4)
 
@@ -88,15 +88,15 @@ contains
 
 !                PRINT *,"error: volume: diff", (abs( SUM(MATMUL(v,(/ (1,j=1,N) /))) &
 !                                             - SUM(MATMUL(result(1,1:n,1:n),(/ (1,j=1,N) /))) ))
-
+!
 !                PRINT *,"error: maxval: diff", ( maxval(result(i,1:n,1:n)) - maxval(v) )
-                return
+
             end do
 
-            curr_max = max_finder_run(maxloc(v), v)
-            PRINT *,"error: better maxval: diff", ( prev_max - curr_max )
-
-            prev_max = curr_max
+!            curr_max = max_finder_run(maxloc(v), v)
+!            PRINT *,"error: better maxval: diff", ( prev_max - curr_max )
+!
+!            prev_max = curr_max
 
             result(i+1,1:n,1:n) = v
             tdata(i+1) = t
